bf6f6e4a1d1c49a33c83b42bdd39aa22446cc787
Andrew Miller <206alm@gmail.com>
2020-10-05 10:11:24 -0700

    add course and hole model, routes for course and controller for course

--- server/config/config.js
+++ server/config/config.js
@@ -28,15 +28,3 @@ module.exports = {
         "dialect": "mysql"
     }
   };
-
-    // HOST: "localhost",
-    // USER: "root",
-    // PASSWORD: "123456789",
-    // DB: "golf_db",
-    // dialect: "mysql",
-    // pool: {
-    //   max: 5,
-    //   min: 0,
-    //   acquire: 30000,
-    //   idle: 10000
-    // }
---
+++ server/models/hole.js
@@ -0,0 +1,40 @@
+const Sequelize = require('sequelize');
+
+
+module.exports = function(sequelize, DataTypes) {
+
+    const Hole = sequelize.define("hole", {
+
+        id: {
+            type: Sequelize.INTEGER(11),
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true
+        },
+
+        par: {
+            type: Sequelize.INTEGER(1),
+            allowNull: false
+        },
+
+        handicap: {
+            type: Sequelize.INTEGER(1),
+            allowNull: false
+        },
+
+        yardage: {
+            type: Sequelize.INTEGER(4),
+            allowNull: false
+        },
+        courseID: {
+            type: Sequelize.INTEGER(11),
+            allowNull: false
+        }
+    })
+
+    Hole.associate = function(models) {
+        Hole.belongsTo(models.course, {foreignKey: 'courseID', as: 'course'})
+    }
+
+    return Hole;
+}
